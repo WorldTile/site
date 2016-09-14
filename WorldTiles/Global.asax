78cd933a3ab64b97f24a84f4b71afa7e90f3bcef
worldtileads <worldtileads@gmail.com>
2016-09-14 23:37:33 +0200

    local commit

---
+++ WorldTiles/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WorldTiles.MvcApplication" Language="C#" %>
---
+++ WorldTiles/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WorldTiles.Startup))]
+namespace WorldTiles
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
